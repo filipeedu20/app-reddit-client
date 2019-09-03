c822ca3a1796c2afaf58e97ab69ee015ba4f0ac9
Filipe Eduardo <filipeedu.bh@gmail.comm>
2019-09-02 23:27:06 -0300

    Primeiro commit

---
+++ pages/api/discurssions/[tag].js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default (req, res) => {
+  const {
+    query: { tag }
+  } = req;
+
+  res.setHeader("Content-Type", "application/json");
+  axios.get(`https://www.reddit.com/r/${tag}.json`).then(({ data }) => {
+    res.send(JSON.stringify(data));
+  });
+};
